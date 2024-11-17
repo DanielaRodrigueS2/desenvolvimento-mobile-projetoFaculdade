e7b0f3b1a7617a6abf293ecfa82a706d08392c29
Marcelo Gregório Vieira <102704462+marcelomgv@users.noreply.github.com>
2024-11-17 17:29:01 -0300

    Update App.js

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ const App = () => {
         <Stack.Screen name='Login' component={Login} options={{headerShown: false}}/>
         <Stack.Screen name='Recuperação de Senha' component={RecuperacaoSenha}/>
         <Stack.Screen name='Nova Conta' component={Register}/>
-        <Stack.Screen name='Ações Pesquisa' component={AcoesPesquisa}/>
+        <Stack.Screen name='Carnaval' component={AcoesPesquisa}/>
         <Stack.Screen name='Coleta' component={Coleta} options={{headerShown: false}}/>
         <Stack.Screen name='Home' component={Home} options={{headerShown: false}}/>
         <Stack.Screen name='Drawer' component={Drawer} options={{headerShown: false}}/>
