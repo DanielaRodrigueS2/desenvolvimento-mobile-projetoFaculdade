df03e70d8b02f277bf42d5c7e64d21a9c6716f32
Marcelo Gregório Vieira <102704462+marcelomgv@users.noreply.github.com>
2024-11-17 17:22:58 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,33 +1,33 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import Login from "./src/screens/Login";
-import Home from "./src/screens/Home";
-import RecuperacaoSenha from "./src/screens/RecuperacaoSenha";
-import Register from "./src/screens/Register";
-import Drawer from "./src/screens/Drawer";
-import NovaPesquisa from "./src/screens/NovaPesquisa";
-import ModificarPesquisa from "./src/screens/ModificarPesquisa";
-import AcoesPesquisa from "./src/screens/AcoesPesquisa";
-import Coleta from "./src/screens/Coleta";
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Login from './src/screens/Login'
+import RecuperacaoSenha from './src/screens/RecuperacaoSenha'
+import Register from './src/screens/Register'
+import AcoesPesquisa from './src/screens/AcoesPesquisa'
+import Coleta from './src/screens/Coleta'
+import Drawer from './src/screens/Drawer'
+import Home from './src/screens/Home'
+import NovaPesquisa from './src/screens/NovaPesquisa'
+import ModificarPesquisa from './src/screens/ModificarPesquisa'
 
 const Stack = createStackNavigator()
 
-const App = () =>{
-
-    return(
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name='Login' component={Login} options={{headerShown: false}}/>
-                <Stack.Screen name='RecuperacaoSenha' component={RecuperacaoSenha} options={{headerShown: false}} />
-                <Stack.Screen name='Register' component={Register} options={{headerShown: false}}/>
-                <Stack.Screen name='Drawer' component={Drawer} options={{headerShown: false}}/>
-                <Stack.Screen name='NovaPesquisa' component={NovaPesquisa} options={{headerShown: true}}/>
-                <Stack.Screen name='ModificarPesquisa' component={ModificarPesquisa} options={{headerShown: true}}/>
-                <Stack.Screen name='AcoesPesquisa' component = {AcoesPesquisa} options={{headerStyle:{backgroundColor: '#2B1D62'},headerTintColor:'#573FBA',headerTitleStyle:{color: '#FFFFFF', fontFamily: 'AveriaLibre-Regular'}}}/>
-                <Stack.Screen name='Coleta' component = {Coleta} options={{headerShown:false}} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+const App = () => {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerTintColor: '#573FBA', headerStyle: { backgroundColor: '#2B1D62'}, headerTitleStyle: {fontFamily: 'AveriaLibre-Regular', fontSize: 30, color: '#FFFFFF'}}}>
+        <Stack.Screen name='Login' component={Login} options={{headerShown: false}}/>
+        <Stack.Screen name='Recuperação de Senha' component={RecuperacaoSenha}/>
+        <Stack.Screen name='Nova Conta' component={Register}/>
+        <Stack.Screen name='Ações Pesquisa' component={AcoesPesquisa}/>
+        <Stack.Screen name='Coleta' component={Coleta} options={{headerShown: false}}/>
+        <Stack.Screen name='Home' component={Home} options={{headerShown: false}}/>
+        <Stack.Screen name='Drawer' component={Drawer} options={{headerShown: false}}/>
+        <Stack.Screen name='Nova Pesquisa' component={NovaPesquisa}/>
+        <Stack.Screen name='Modificar Pesquisa' component={ModificarPesquisa}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 export default App
